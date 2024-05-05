186c6239f28f7185a8d2edae2962685ea4691bd3
muhammadwahabarifin <wahabmuhammad691@gmail.com>
2024-05-06 01:05:07 +0700

    store

---
+++ src/components/Footer.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer id="footer">
+        <div className="container">
+          <a
+            href="https://github.com/muhammadwahabarifin"
+            className="repo_link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            github.com/wahabarf/redux-shopping-cart
+          </a>
+
+          <p>
+            Built by | &nbsp;
+            <a
+              href="https://gulshansongara.netlify.app"
+              target="_blank"
+              rel="noreferrer"
+            >
+              WahabArf
+            </a>
+          </p>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
---
+++ src/pages/Home.js
@@ -0,0 +1,22 @@
+import React from "react";
+import productsData from "../data/productsData";
+import ProductsCard from "../components/ProductsCard";
+
+const Home = () => {
+  return (
+    <>
+    <section id="home" className="bg_grad">
+      <div className="container">
+        <div className="home_content">
+          {
+            productsData.map((item) => (
+              <ProductsCard key={item.id} {...item} />
+            ))
+          }
+        </div>
+      </div>
+    </section>
+    </>
+  );
+};
+export default Home;
